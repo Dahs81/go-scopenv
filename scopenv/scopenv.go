dcdc88c545c4d60ab18577eb86590fbab710bce8
Shad Beard <sbeard@ensequence.com>
2015-01-07 17:05:20 -0500

    Adding some checks and panicking

--- scopenv/scopenv.go
+++ scopenv/scopenv.go
@@ -1,6 +1,7 @@
 package scopenv
 
 import (
+	"fmt"
 	"os"
 	"strings"
 )
@@ -8,7 +9,7 @@ import (
 // Scopenv - REDEFINE CREATE : Maybe also return error
 func Scopenv(m []string, v ...string) map[string]string {
 
-	r := reverse(v)
+	r, _ := reverse(v)
 	s := make(map[string]string)
 
 	scope := find(s, m, r)
@@ -17,13 +18,18 @@ func Scopenv(m []string, v ...string) map[string]string {
 }
 
 func find(s map[string]string, m, rev []string) map[string]string {
-
 	for i := range m {
 		tmp := strings.Join(rev, "_")
 		f := upper(tmp) + "_" + upper(m[i])
+		if len(rev) == 0 {
+			panic(fmt.Sprintf("Error: you are missing a env variable for one of the values in m."))
+		}
+		f = strings.Replace(f, "_", "", 1)
+
 		check := os.Getenv(f)
 		if check == "" {
 			rev = removeFirst(rev)
+
 			find(s, m, rev)
 		} else {
 			s[m[i]] = check
@@ -34,13 +40,15 @@ func find(s map[string]string, m, rev []string) map[string]string {
 }
 
 // reverse - reverses the order of a slice of strings
-func reverse(v []string) []string {
+func reverse(v []string) ([]string, int) {
 	var r []string
 	for i := range v {
 		l := v[len(v)-1-i]
 		r = append(r, l) // Suggestion: do `last := len(s)-1` before the loop
+
 	}
-	return r
+	leng := len(r)
+	return r, leng
 }
 
 // upper - Helper function for capitalizing arguments
